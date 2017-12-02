e8cd2442545fc37c10236ad37ea3c4ed5fc068b2
Jack boyce <boyce.j3131@gmail.com>
2017-12-02 12:20:34 -0500

    added text to speach and NLP

---
+++ YHackApp/Credentials.swift
@@ -0,0 +1,14 @@
+//
+//  Credentials.swift
+//  YHackApp
+//
+//  Created by Jack Boyce on 12/2/17.
+//  Copyright © 2017 Jack Boyce. All rights reserved.
+//
+
+import Foundation
+
+struct Credentials {
+    static let SpeechToTextUsername = "7a25bb15-47cf-4964-8671-cd76c5dba4ac"
+    static let SpeechToTextPassword = "wiiwACUriU0Y"
+}
--- YHackApp/ViewController.swift
+++ YHackApp/ViewController.swift
@@ -7,12 +7,60 @@
 //
 
 import UIKit
+import SpeechToTextV1
+//import NSLinguisticTagger
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var outputTextView: UITextView!
+    @IBOutlet weak var recordTextView: UITextView!
+
+    var speechToText: SpeechToText!
+    var speechToTextSession: SpeechToTextSession!
+    var isStreaming = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        speechToText = SpeechToText(
+            username: Credentials.SpeechToTextUsername,
+            password: Credentials.SpeechToTextPassword
+        )
+        speechToTextSession = SpeechToTextSession(
+            username: Credentials.SpeechToTextUsername,
+            password: Credentials.SpeechToTextPassword
+        )
+        print("loaded")
+//        let text = "The American Red Cross was established in Washington, D.C., by Clara Barton."
+//        let tagger = NSLinguisticTagger(tagSchemes: ["nameType"], options: 0)
+//        tagger.string = "The American Red Cross was established in Washington, D.C., by Clara Barton."
+//        let range = NSRange(location:0, length: text.utf16.count)
+//        let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace, .joinNames]
+//        let tags: [String] = ["personalName", "placeName", "organizationName"]
+//        tagger.enumerateTags(in: range, scheme: "nameType", options: options) { tag, tokenRange, otherRange, stop in
+//            print("Inside")
+//            if tag != "" && tags.contains(tag) {
+//                let name = (text as NSString).substring(with: tokenRange)
+//                print("\(name): \(tag)")
+//            }
+//            if tag != "" {
+//                print(tag)
+//            }
+//            
+//        }
+        
+    }
+    
+    public func textToPOS(str: String) {
+        let options: NSLinguisticTagger.Options = [.omitWhitespace, .omitPunctuation, .joinNames]
+        let schemes = NSLinguisticTagger.availableTagSchemes(forLanguage: "en")
+        let tagger = NSLinguisticTagger(tagSchemes: schemes, options: Int(options.rawValue))
+        tagger.string = str
+        tagger.enumerateTags(in: NSMakeRange(0, (str as NSString).length), scheme: NSLinguisticTagSchemeNameTypeOrLexicalClass, options: options) { (tag, tokenRange, _, _) in
+            let token = (str as NSString).substring(with: tokenRange)
+            //print("\(token): \(tag)")
+            outputTextView.text! += "\(token): \(tag)\n"
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +68,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var microphoneButton: UIButton!
+    public func streamMicrophoneBasic() {
+        if !isStreaming {
+            
+            // update state
+            microphoneButton.setTitle("Stop Microphone", for: .normal)
+            isStreaming = true
+            
+            // define recognition settings
+            var settings = RecognitionSettings(contentType: .opus)
+            settings.interimResults = true
+            //settings.keywordsThreshold = 0.45
+            settings.wordAlternativesThreshold = 0.45
+            settings.speakerLabels = true
+            
+            // define error function
+            let failure = { (error: Error) in print(error) }
+            
+            // start recognizing microphone audio
+            speechToText.recognizeMicrophone(settings: settings, failure: failure) {
+                results in
+                self.recordTextView.text = results.bestTranscript
+                self.outputTextView.text = ""
+                self.textToPOS(str: self.recordTextView.text)
+            }
+            
+        } else {
+            
+            // update state
+            microphoneButton.setTitle("Start Microphone", for: .normal)
+            isStreaming = false
+            
+            // stop recognizing microphone audio
+            speechToText.stopRecognizeMicrophone()
+            
+            outputTextView.text = ""
+            textToPOS(str: recordTextView.text)
+        }
+    }
 
+    @IBAction func didPressMicrophoneButton(_ sender: Any) {
+        streamMicrophoneBasic()
+    }
 }
 
